3af5c64acaa4724a6d95a74c29590b1126e635a3
Tanya <61435683+vermatanya@users.noreply.github.com>
2022-01-28 00:07:16 +0530

    Create Main.java
    ArrayList 1

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+public static void main(String[] args) {
+    List<String> l1 = new ArrayList<String>();
+    l1.add("tan");
+    l1.add("Saum");
+    l1.add("Sav");
+    l1.add("Yog");
+    
+    System.out.println(l1);
+    for(int i=0;i<l1.size();i++){
+        System.out.println(l1.get(i));
+    }
+    
+}
+}
