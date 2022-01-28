0a573e429a908ae315f91978569c3e4bba3a40f5
Tanya <61435683+vermatanya@users.noreply.github.com>
2022-01-28 19:13:36 +0530

    ArrayList : Shuffling elements

---
+++ ArrList10.java
@@ -0,0 +1,17 @@
+/*Write a Java program to shuffle elements in a array list. */
+import java.util.*;
+public class ArrList10{
+    public static void main(String[] args){
+        List<String> l1 = new ArrayList<String>();
+        l1.add("apple");
+        l1.add("banana");
+        l1.add("grapes");
+        l1.add("mango");
+        System.out.println("List 1: "+l1);
+
+        Collections.shuffle(l1);
+        System.out.println("After shuffling: ");
+        System.out.println("List 1: "+l1);
+
+    }
+}
